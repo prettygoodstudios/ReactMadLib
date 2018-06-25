90edc1aca848ed776c81fdf8765665aaa81e589e
Miguel Rust <hava12soft@gmail.com>
2018-06-25 08:44:11 -0600

    Syled input grid.

--- src/components/input.js
+++ src/components/input.js
@@ -2,7 +2,7 @@ import React, {Component} from 'react';
 const Imput = ({title,color,name},change,key) => {
   return(
     <div className="input" key={key}>
-      <label className="input-label">{`${key+1}.`}</label>
+      <label className="input-number">{`${key+1}`}</label>
       <input value={color} onChange={change} name={name}/>
       <label className="input-title">{title}</label>
     </div>
